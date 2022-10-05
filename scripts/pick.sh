18d611e304a9aa3e049081f9594155a422664c41
raoxuanzzz <raoxuanzzz@gmail.com>
2022-10-05 23:15:57 +0800

    modify

--- scripts/pick.sh
+++ scripts/pick.sh
@@ -1,3 +1,25 @@
+#!/bin/bash
+#SBATCH --job-name=ddppo_pick
+#SBATCH --output=ddppo_pick.out
+#SBATCH --error=ddppo_pick.err
+#SBATCH --gres=gpu:v100:1
+#SBATCH --cpus-per-task 10
+#SBATCH --nodes 1
+#SBATCH --mem=120GB
+#SBATCH --time=96:30:00
+#SBATCH --constraint=[gpu]
+#SBATCH --partition=batch
+
+export MAGNUM_LOG=quiet
+export HABITAT_SIM_LOG=quiet
+
+
+module load cudnn/7.5.0-cuda10.1.105
+module load nccl/2.4.8-cuda10.1
+source /home/hanp/anaconda3/bin/activate rx_habitat
+cd /home/hanp/hanp/research/UESTC/xuan_rao/code/habitat-lab
+
+set -x
 python habitat_baselines/run.py \
     --exp-config habitat_baselines/config/rearrange/ddppo_pick.yaml \
     --run-type train \
